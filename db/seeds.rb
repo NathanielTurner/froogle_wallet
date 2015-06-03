59d5f973ec1c46bf7955e47abc46c366f8558680
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-06-03 19:49:54 -0400

    Build test seeds for withdrawals and deposits.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,17 +2,12 @@ how_many = 10
 
 how_many.times do |i|
   Withdrawal.create!(collector: Faker::Company.bs,
-      district_at: Faker::Address.city,
-      hometown_at: Faker::Address.city,
-      party: parties.sample)
+      amount: Faker::Number.between(from = 1.00, to = 500.00),
+      date: Faker::Date.between(2.days.ago, Date.today),
 end
 
-cwithdrawal = Candidate.all
 how_many.times do
-  v = Voter.create!(name: Faker::Name.name,
-      party: parties.sample)
-  Vote.create(voter_id: v.id,
-      candidate_id: candidates.sample.id)
+  Deposit.create!(investor: Faker::Name.name,
+      amount: Faker::Number.between(from = 1.00, to = 5000.00),
+      date: Faker::Date.between(2.days.ago, Date.today)
 end
-
-puts "This took #{Time.now-start_time} seconds"
