08ebe7167fc8fbc3ec181bdba170c16710f9dbe1
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-06-03 21:36:09 -0400

    All scaffold generated tests pass.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ how_many = 10
 how_many.times do |i|
   Withdrawal.create!(amount: Faker::Number.between(from = 1.00, to = 500.00),
                      date: Faker::Date.between(2.days.ago, Date.today),
-                     collector: Faker::Company.bs
+                     collector: Faker::Company.bs)
 end
 
 how_many.times do
